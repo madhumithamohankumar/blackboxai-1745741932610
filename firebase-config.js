b611f733bab62830b1f58a69b8b74de9ab3b3aa5
madhumithamohankumar <madhumithamohankumar@gmail.com>
2025-04-27 13:49:37 +0530

    Add firebase-config.js 1745741978903

---
+++ firebase-config.js
@@ -0,0 +1,15 @@
+// firebase-config.js - Firebase configuration placeholder
+
+// Replace the below config object with your Firebase project credentials
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  databaseURL: "https://YOUR_PROJECT_ID.firebaseio.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT_ID.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
